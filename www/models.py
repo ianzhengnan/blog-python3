f971b7b472e2814575172c3bdf634ad921f0304d
IanZheng <flks_2005@qq.com>
2016-03-31 00:06:23 +0800

    finish models

--- www/models.py
+++ www/models.py
@@ -9,7 +9,7 @@ __author__ = 'Ian Zheng'
 
 import time, uuid
 
-from www.orm import Model, StringField, BooleanField, IntegerField, FloatField
+from www.orm import Model, StringField, BooleanField, IntegerField, FloatField, TextField
 
 def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
@@ -27,4 +27,30 @@ class User(Model):
 
 class Blog(Model):
     __table__ = 'blogs'
+
+    id = StringField(primary_key=True, defalut=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+
+
+
+
+
 
