f0bd67bdcd270e7ab63cc736833e97ebeccec18c
IanZheng <flks_2005@qq.com>
2016-04-04 21:39:30 +0800

    About REST API (web API)

--- www/handlers.py
+++ www/handlers.py
@@ -27,5 +27,13 @@ def index(request):
         'blogs': blogs
     }
 
+@get('/api/users')
+def api_get_users():
+    users = yield from User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '*********'
+
+    return dict(users = users)
+
 
 
